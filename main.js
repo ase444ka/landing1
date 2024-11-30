aefe807527ed5ff8d3a43a1ba35d3f883f2447b1
Ase444ka <istomina.asia@yandex.ru>
2024-11-30 18:28:14 +0300

    add reset

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // import './assets/sass/info.scss'
 
-const modules = import.meta.glob(['./assets/sass/*.scss'], { eager: true })
+const modules = import.meta.glob(['./assets/styles/*.scss'], { eager: true })
 console.log(modules)
 export default modules
